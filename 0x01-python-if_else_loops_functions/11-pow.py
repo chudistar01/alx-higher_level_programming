a6a93373e25b13324e216cf9070f13e2eab1f6b8
chudistar01 <christianigwebuike31@gmail.com>
2024-01-16 20:58:23 -0800

    statements

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+"""computes a to the power of b and return the value"""
+
+
+def pow(a, b):
+    return (a ^ b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+""" prints the last digit of a number"""
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return(abs(number) % 10)
