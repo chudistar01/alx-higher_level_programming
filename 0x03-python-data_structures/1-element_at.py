f3d6a3af1b87bbc0e8b216522ded349b211fd1e4
chudistar01 <christianigwebuike31@gmail.com>
2024-01-22 18:50:41 -0800

    python structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element_at(my_list, idx):
     for i in range(list_length):
         if idx > 0:
                 if idx == my_list[i]:
-                    return(my_list[idx])
+                    return my_list[idx]
