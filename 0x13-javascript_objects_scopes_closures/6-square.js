d22b78cf0078980c859dc8d168c281f4ac00b5da
chudistar01 <christianigwebuike31@gmail.com>
2024-04-24 21:41:12 -0700

    warm up

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Square_ = require('./5-square.js');
+
+class Square extends Square_ {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+module.exports = Square;
