4e9f568433f3e8aed3d225af938ca0ecd19c7248
chudistar01 <christianigwebuike31@gmail.com>
2024-05-08 16:52:56 -0700

    python classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-
 """Write a class Square that defines a square by"""
+
+
 class Square:
+    """initialization of size"""
     def __init__(self, size=0):
         self.size = size
 
     @property
     def size(self):
+        """returns size private attribute"""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
+        """private attribute setter"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
+        """returns the area of the square"""
         return (self.__size ** 2)
