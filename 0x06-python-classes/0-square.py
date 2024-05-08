2c7ee8a62ff6a3e317814e7e7d6a4ebf42dc8e75
chudistar01 <christianigwebuike31@gmail.com>
2024-05-08 16:00:05 -0700

    python classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Define class Square"""
 
-"""Write an empty class Square
-that defines a square"""
 
 class Square:
+    """Represent a square"""
     pass
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,8 @@
+#!/ussr/bin/python3
+
+
+class Square:
+    def __str_(self):
+        return self.pos_print()[:-1]
+
+    def 
