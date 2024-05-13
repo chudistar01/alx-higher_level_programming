e446d488e9d53dd52cfd70af479e29c569342109
chudistar01 <christianigwebuike31@gmail.com>
2024-05-13 15:39:56 -0700

    networks

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that displays all HTTP methods
-curl -sI "$1" | grep "Allow" | awk '{print "$2"}' | tr -d '\r'
+curl -sI "$1" | grep -i "Allow" | awk '{print "$2"}' | tr -d '\r'
