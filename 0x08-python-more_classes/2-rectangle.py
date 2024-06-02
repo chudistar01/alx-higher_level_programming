a474f0252963b1cc5358a5e21eb77d9360f6cbde
chudistar01 <christianigwebuike31@gmail.com>
2024-06-01 19:51:52 -0700

    python classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,7 +5,8 @@ Rectangle class
 
 
 class Rectangle:
-    """Represent a rectangle"""
+    """Represent a rectangle
+    """
 
     def __init__(self, width=0, height=0):
         """initializes a new rectangle.
