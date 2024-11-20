1407a09645180bf0217233114d455659e69a5042
chudistar01 <christianigwebuike31@gmail.com>
2024-11-20 07:30:35 -0800

    classes

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,44 +1,53 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square by"""
+
 
 class Square:
+    """initialization of size"""
     def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
-        return self.__size
+        """returns size private attribute"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
+        """private attribute setter"""
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        return self.__size * self.__size
-    
-
-    def __le__(self, other):
-        return self.area() <= other.area()
-
+        """returns the area of the square"""
+        return (self.__size ** 2)
 
     def __lt__(self, other):
-        return self.area() < other.area()
+        """comparison"""
+        return self.__size < other.__size
 
-
-    def __ge__(self, other):
-        return self.area() >= other.area()
-
-
-    def __ne__(self, other):
-        return self.area() != other.area()
-
-
-    def __gt__(self, other):
-        return self.area() > other.area()
-
+    def __le__(self, other):
+        """comparison"""
+        return self.__size <= other.__size
 
     def __eq__(self, other):
-        return self.area() == other.area()
+        """comparison"""
+        return self.__size == other.__size
+
+    def __ne__(self, other):
+        """comparison"""
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        """comparison"""
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        """comparison"""
+        return self.__size >= other.__size
+
+
+
